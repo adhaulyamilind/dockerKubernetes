9823779698c5ff9e12dc61ec52fd05e90e7d8c4a
Milind Adhaulya <milindadhaulya@Milinds-MacBook-Pro.local>
2021-01-11 15:03:23 +0530

    minor spelling change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Milind Adhaulya Bye <code>src/App.js</code> and save to reload.
+           Milind Adhaulya Check <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
